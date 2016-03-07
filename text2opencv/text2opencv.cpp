1656878fd71fdc16ce04d04d1f4fbb3f8c69f9c3
Lily <408310996@qq.com>
2016-03-07 22:20:57 +0800

    添加金字塔，方便对图像进行缩放

--- text2opencv/text2opencv.cpp
+++ text2opencv/text2opencv.cpp
@@ -1,4 +1,7 @@
 
+/*3Dreconstruction
+2016.3.7 加入SIFT算法识别图像特征
+*/
 #include "stdafx.h"
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -9,20 +12,56 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	cvNamedWindow("src",1);
-	Mat input=imread("pic//IMG_5595.jpg");
-	Mat img_threshold;
+	//cvNamedWindow("src",1);
+	Mat input1=imread("pic//IMG_5595.jpg");
+	Mat input2=imread("pic//IMG_5594.jpg");
+	Mat temp=input1;
+	if(!input1.data || !input2.data)
+	{
+		printf("Read image data Failure\n");
+		return 1;
+	}
+	namedWindow("src",CV_WINDOW_AUTOSIZE);
+
+
 	cv::Size picSize;
-	picSize.height=100;
-	picSize.width=150;
-	resizeWindow("src",150,100);
-	resize(input,input,picSize);
-	//threshold(input,img_threshold,60,255,CV_THRESH_BINARY_INV);
-	imshow("src",input);
+	picSize.height=80;
+	picSize.width=80;
+
+	resize(input1,input1,picSize);
+	resize(input2,input2,picSize);
+
+	//resizeWindow("src",50,80);
 	cvWaitKey(0);
-	Mat img_contours;
-	img_threshold.copyTo(img_contours);
-	vector<vector<Point>> contours;
+	while( true )
+	{
+		int c;
+		c=waitKey(10);
+		if( (char)c ==27)
+		{
+			break;
+		}
+		else if((char)c=='u')
+		{
+			pyrUp(temp,input1,Size(temp.cols*2,temp.rows*2));
+			printf("** Zoom In : Image*2\n");
+		}
+		else if((char)c=='d')
+		{
+			pyrDown(temp,input1,Size(temp.cols/2,temp.rows/2));
+			printf("** Zoom In : Image/2\n");
+		}
+		imshow("src",input1);
+		temp=input1;
+	}
+	cvWaitKey(0);
+	//threshold(input,img_threshold,60,255,CV_THRESH_BINARY_INV);
+
+
+	//	Mat img_threshold;
+	//Mat img_contours;
+	//img_threshold.copyTo(img_contours);
+	//vector<vector<Point>> contours;
 //	findContours(img_contours,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
 	//vector<int> test_data;
 	//vector<int>::iterator check;
