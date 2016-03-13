a2f5e520fdfc020752b836138e037602b90a109b
Lily <408310996@qq.com>
2016-03-13 23:10:08 +0800

    添加git忽略文件 添加sift识别特征

--- text2opencv/text2opencv.cpp
+++ text2opencv/text2opencv.cpp
@@ -1,13 +1,16 @@
 
 /*3Dreconstruction
-2016.3.7 加入SIFT算法识别图像特征
+2016.3.7 加入SIFT算法识别图像特征 Scale invaluan feature 
 */
 #include "stdafx.h"
+#include <iostream>
+#include <conio.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <vector>
 #include <opencv2\features2d\features2d.hpp>
 #include <opencv2/xfeatures2d.hpp>
+#include <opencv2/xfeatures2d/nonfree.hpp>
 
 using namespace cv;
 using namespace std;
@@ -16,37 +19,62 @@ void changeSizetoSame(Mat & inputImage);
 int _tmain(int argc, _TCHAR* argv[])
 {
 //	Ptr<SIFT> detector = SIFT::create();
-	Mat input1=imread("pic//IMG_5597.jpg",IMREAD_GRAYSCALE );
+	Mat input1=imread("pic//IMG_5597.jpg" );
 	Mat input2=imread("pic//IMG_5598.jpg");
+	//imshow("src",input2);
+	//cvWaitKey(0);
 	Mat descriptors;
 	vector<KeyPoint> keypoints;
 	//initModule_nonfree();
+
 	if(!input1.data || !input2.data)
 	{
 		printf("Read image data Failure\n");
 		return 1;
 	}
 	//changeSizetoSame(input1);
-	imshow("src",input2);
-	cvWaitKey(0);
+	
+	//cvWaitKey(0);
+	//SiftFeatureDetector detector;
 	cv::Ptr<Feature2D> f2d = xfeatures2d::SIFT::create();
 	//threshold(input,img_threshold,60,255,CV_THRESH_BINARY_INV);
-
+	
 	std::vector<KeyPoint> keypoints_1, keypoints_2;    
-  f2d->detect( input1, keypoints_1 );
-  f2d->detect( input2, keypoints_2 );
+	f2d->detect( input1, keypoints_1 );
+	f2d->detect( input2, keypoints_2 );
+	cout<<"cout<<keypoints_1.size()"<<keypoints_1.data()<<endl;
+	cout<<"cout<<keypoints_2.size()"<<keypoints_2.size()<<endl;
 
-  //-- Step 2: Calculate descriptors (feature vectors)    
-  Mat descriptors_1, descriptors_2;    
-  f2d->compute( input1, keypoints_1, descriptors_1 );
-  f2d->compute( input2, keypoints_2, descriptors_2 );
-
-  //-- Step 3: Matching descriptor vectors using BFMatcher :
-  BFMatcher matcher;
-  std::vector< DMatch > matches;
-  matcher.match( descriptors_1, descriptors_2, matches );
+	  //-- Step 2: Calculate descriptors (feature vectors)    
+	Mat descriptors_1, descriptors_2;    
+	f2d->compute( input1, keypoints_1, descriptors_1 );
+	f2d->compute( input2, keypoints_2, descriptors_2 );
+	cout<<"cout<<keypoints_1.size()"<<descriptors_1.size()<<endl;
+	  //-- Step 3: Matching descriptor vectors using BFMatcher :
+	BFMatcher matcher;
+	std::vector< DMatch > matches;
+	matcher.match( descriptors_1, descriptors_2, matches );
+	cout<<"cout<<keypoints_1.size()"<<matches.size()<<endl;
+	Mat outImg=input1;
+	Mat outImg2;
+	vector<Mat> input;
+	vector< vector<KeyPoint> >inputkey;
+	input.push_back(input1);
+	input.push_back(input2);
+	inputkey.push_back(keypoints_1);
+	inputkey.push_back(keypoints_2);
+	drawKeypoints(input1,keypoints_1,outImg, Scalar::all(-1),DrawMatchesFlags::DEFAULT);
+	imshow("hello",outImg);
+	cvWaitKey(0);
+	drawMatches(input1,keypoints_1,input1,keypoints_1,matches,outImg2);
+	imshow("src",outImg2);
 //	Ptr<Feature2D> sift = Algorithm::create<Feature2D>("Feature2D.SIFT");
-	destroyAllWindows();
+	cvWaitKey(0);
+	destroyWindow("hello");
+	destroyWindow("src");
+	//system("pause");
+	
+	getch();
 	return 0;
 }
 
